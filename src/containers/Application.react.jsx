35aca0fb3bed2738749b096de6ddf65ab4d95969
Raito Bezarius <masterancpp@gmail.com>
2015-10-25 20:15:46 +0100

    Apply the styles to the application wide container

--- src/containers/Application.react.jsx
+++ src/containers/Application.react.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
 
+import classNames from 'classnames';
+
 import Navbar from 'containers/Navbar.react';
 import Footer from 'containers/Footer.react';
 
+import ApplicationStyleStore from 'stores/ApplicationStyleStore';
+import connectToStores from 'alt/utils/connectToStores';
+
 import './Application.react.css';
 
+@connectToStores
 export default class Application extends React.Component {
+  static propTypes = {
+    currentClasses: React.PropTypes.object.isRequired
+  }
+  static getStores() {
+    return [ApplicationStyleStore];
+  }
+  static getPropsFromStores() {
+    return ApplicationStyleStore.getState();
+  }
 	render() {
 		return (
-      <main className="portfolio-container">
+      <main className={classNames('portfolio-container', this.props.currentClasses)}>
 				<Navbar />
 				<RouteHandler />
 				<Footer />
