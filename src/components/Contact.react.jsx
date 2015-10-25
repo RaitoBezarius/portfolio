08f915039e6053d2825040805d58964d537220c4
Raito Bezarius <masterancpp@gmail.com>
2015-10-25 20:16:02 +0100

    Fix responsiveness of the Contact page

--- src/components/Contact.react.jsx
+++ src/components/Contact.react.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
+import ApplicationStyleActions from 'actions/ApplicationStyleActions';
 import ImageOverlay from 'components/ui/ImageOverlay.react';
 import View from 'react-flexbox';
 import './Contact.react.css';
 
 export default class Contact extends React.Component {
+  componentDidMount() {
+    ApplicationStyleActions.applyClass('app-space-background');
+  }
+  componentWillUnmount() {
+    ApplicationStyleActions.removeClass('app-space-background');
+  }
   render() {
     return (
-      <section className="space-overwrite">
-        <ImageOverlay imageSrc={require('assets/images/space.jpg')} imageAlt='Space background image'>
-          <View auto column style={{'justifyContent': 'flex-start'}} className="contact-box">
-            <hgroup>
-              <h1 className="contact-head">Under pressure, you are waiting for directions.</h1>
-            </hgroup>
-            <button className="contact-cta">Stop thinking, let's work together.</button>
-          </View>
-        </ImageOverlay>
+      <section className="contact-section">
+        <hgroup>
+          <h1 className="contact-head">Under pressure, you are waiting for directions.</h1>
+        </hgroup>
+        <button className="contact-cta">Stop thinking, let's work together.</button>
       </section>
     );
   }
