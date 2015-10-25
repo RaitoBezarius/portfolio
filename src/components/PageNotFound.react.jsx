5cc345eae5e175688636e41830daee22affebffa
Raito Bezarius <masterancpp@gmail.com>
2015-10-25 02:55:41 +0200

    Add a 404 page

---
+++ src/components/PageNotFound.react.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class PageNotFound extends React.Component {
+  render() {
+    return (
+      <p>OMAGAD I CANNOT FOUND THIS PAGE</p>
+    );
+  }
+}
--- src/core/Router.jsx
+++ src/core/Router.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import Router, {Route, DefaultRoute} from 'react-router';
+import Router, {Route, DefaultRoute, NotFoundRoute} from 'react-router';
 
 import Application from 'containers/Application.react';
 import Landing from 'components/Landing.react';
 import Contact from 'components/Contact.react';
 import Projects from 'components/Projects.react';
+import PageNotFound from 'components/PageNotFound.react';
 
 const Routes = (
   <Route handler={Application}>
     <DefaultRoute handler={Landing} />
+    <NotFoundRoute handler={PageNotFound} />
     <Route handler={Landing}
       name="home" />
     <Route handler={Contact}
